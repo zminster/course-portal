6a4438b1efc61c4828578716cb831b655e2a66ef
Zach Minster <zminster@stab.org>
2016-07-29 20:49:03 -0400

    production/dev http/https dynamic run enabled

--- server.js
+++ server.js
@@ -12,26 +12,28 @@ var flash		= require('connect-flash');
 var lex			= require('letsencrypt-express').testing();
 
 // consts
-//var PORT 		= 8080;
-var DOMAIN		= "cs.stab.org";
-var EMAIL		= "zminster@stab.org";
+var DEV_PORT 	= 8080;
+var DOMAIN		= 'cs.stab.org';
+var EMAIL		= 'zminster@stab.org';
 
 'use strict';
 
 // init
 var app 		= express();
-var lex = lex.create({
-	configDir: require('os').homedir() + '/letsencrypt/etc',
-	approveRegistration: function (hostname, approve) {
-		if (hostname === DOMAIN) {
-			approve(null, {
-				domains: [DOMAIN],
-				email: EMAIL,
-				agreeTos: true
-			});
+if (process.env.NODE_ENV == 'production') {
+	var lex = lex.create({
+		configDir: require('os').homedir() + '/letsencrypt/etc',
+		approveRegistration: function (hostname, approve) {
+			if (hostname === DOMAIN) {
+				approve(null, {
+					domains: [DOMAIN],
+					email: EMAIL,
+					agreeTos: true
+				});
+			}
 		}
-	}
-});
+	});
+}
 
 app.use(morgan('dev'));
 
@@ -40,9 +42,19 @@ app.get("/", function(req, res) {
 	res.end();
 });
 
-lex.onRequest = app;
 
-lex.listen([80], [443, 5001], function () {
-	var protocol = ('requestCert' in this) ? 'https': 'http';
-	console.log("Listening at " + protocol + '://localhost:' + this.address().port);
-});
+// production: 80/443/5001 (SSL enabled)
+if (process.env.NODE_ENV == 'production') {
+	lex.onRequest = app;
+
+	lex.listen([80], [443, 5001], function () {
+		var protocol = ('requestCert' in this) ? 'https': 'http';
+		console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+	});
+}
+// dev: 8080 (SSL disabled)
+else {
+	app.listen(DEV_PORT, function() {
+		console.log("Listening at http://localhost:8080");
+	})
+}
