f99a85a577dcf4f1b1774f2d352e2fffbca4a6e0
Zach Minster <zminster@stab.org>
2016-08-16 14:01:41 -0400

    fixing multer asynch issue

--- app/upload.js
+++ app/upload.js
@@ -8,7 +8,6 @@ var handinDir 	= '/course/csp/handin/';
 var multer		= require('multer');
 var fs 			= require('fs');
 var mkdirp		= require('mkdirp');
-var exec 		= require('child_process').exec;
 var storage = multer.diskStorage({
 	// send uploads to correct handin dir thorugh multer
 	destination: function (req, file, callback) {
@@ -18,13 +17,8 @@ var storage = multer.diskStorage({
 		else {
 			var handin_path = handinDir + req.params.asgn_id + '/' + req.user.username;
 			mkdirp(handin_path, {mode: 0770}, function(err, made) {
-				if (!err) {	// delete any existing files
-					exec('rm -rf ' + handin_path + '/*', function (err, stdout, stderr) {
-						if (!err)
-							callback(null, handin_path);
-						else
-							callback(err, null);
-						});
+				if (!err) {
+					callback(null, handin_path);
 				} else
 					callback(err, null);
 			});
