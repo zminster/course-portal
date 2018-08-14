14f2383fc22be9a3cdebdd599a34e1bb5647955a
Zach Minster <zminster@stab.org>
2018-08-14 00:22:26 -0400

    resolves #23

--- app/routes.js
+++ app/routes.js
@@ -510,6 +510,6 @@ function checkPassword(str)
 {
 	// at least one number, one lowercase and one uppercase letter
 	// at least six characters that are letters, numbers or the underscore
-	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(.*){6,}$/;
 	return re.test(str);
 }
--- public/js/password.js
+++ public/js/password.js
@@ -32,7 +32,7 @@ function checkMatch (e) {
 function checkPassword(str)
 {
 	// at least one number, one lowercase and one uppercase letter
-	// at least six characters that are letters, numbers or the underscore
-	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+	// at least six characters
+	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(.*){6,}$/;
 	return re.test(str);
 }
