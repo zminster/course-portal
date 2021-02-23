140aa78710189f97f4e4feeb5790af3aa65b6517
root <root@cs.stab.org>
2021-02-23 14:51:13 -0500

    email working

--- emailservice/server.js
+++ emailservice/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config({path: __dirname + '../.env'});
 const mysql = require('mysql2');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 const nodemail = require('nodemailer');
 const {
 	uniqueNamesGenerator,
@@ -12,9 +12,9 @@ const {
 
 //connect to db
 const connection = mysql.createConnection({
-	host: process.env.HOST,
-	database: process.env.DATABASE,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
 	user: process.env.DB_USER,
 	insecureAuth: true
 });
@@ -27,12 +27,12 @@ connection.connect((err) => {
 		dictionaries: [adjectives, animals, names],
 		style: 'lowerCase'
 	});
-	console.log("running query", process.env.HOST, process.env.DB_USER);
+	console.log("running query", process.env.DB_HOST, process.env.DB_USER);
 	//grab information, and send all the people who are missing assignments
 	let transporter = nodemail.createTransport({
 		sendmail: true,
 		newline: 'unix',
-		path: 'usr/sbin/sendmail'
+		path: '/usr/sbin/sendmail'
 	});
 	let date = new Date().getTime();
 	connection.query("SELECT grades.uid, grades.asgn_id, grades.extension, membership.class_pd, assignment_meta.date_due FROM grades INNER JOIN membership ON grades.uid = membership.uid " +
@@ -53,27 +53,29 @@ connection.connect((err) => {
 						connection.query("SELECT assignment.name, assignment.url, user_meta.first_name, user_meta.last_name, user_meta.email FROM assignment, user_meta WHERE assignment.asgn_id=? AND user_meta.uid=?", [item.asgn_id, item.uid], (err, assignmentRow) => {
 							if (err) console.log("selection from assignment for name and url", err);
 							//fill out the text file <--must be named late.txt
-							res.json(assignmentRow);
 							if (index < 20) {
 								console.log("send email");
 								fs.readFile(path.join(__dirname, "assignmentFiles", "late1.txt"), function(err, template) {
 									if (err) console.log(err);
+									if (assignmentRow[0].first_name == "Sophia" && assignmentRow[0].last_name == "Manavalan") {
+										console.log(assignmentRow);
 									let dateTime = new Date(udate).toString().substring(0, 15);
 									let returnEmail = template.toString();
-									returnEmail = returnEmail.replace("{{SPECIMEN_NAMEF}}", userInfoRow[0].first_name);
-									returnEmail = returnEmail.replace("{{SPECIMEN_NAMEL}}", userInfoRow[0].last_name);
+									returnEmail = returnEmail.replace("{{SPECIMEN_NAMEF}}", assignmentRow[0].first_name);
+									returnEmail = returnEmail.replace("{{SPECIMEN_NAMEL}}", assignmentRow[0].last_name);
 									returnEmail = returnEmail.replace("{{ASSIGNMENT_NAME}}", assignmentRow[0].name);
 									returnEmail = returnEmail.replace("{{DUE_DATE}}", dateTime ? dateTime : "");
 									returnEmail = returnEmail.replace("{{URL}}", assignmentRow[0].url ? assignmentRow[0].url : "");
-									console.log("EMAIL SEND TO:", userInfoRow[0].email);
+									console.log("EMAIL SEND TO:", assignmentRow[0].email);
 									transporter.sendMail({
-										from: '"CSP" ' + randString + '@cs.stab.org',
-										to: userInfoRow[0].email,
+										from: 'CSP' + randString + '@cs.stab.org',
+										to: "smorris21@students.stab.org",
 										subject: 'Missing Assignment: ' + assignmentRow[0].name,
 										html: "<pre>" + returnEmail + "</pre>"
 									}, (err, info) => {
-										console.error();
+										console.error(err);
 									});
+									}
 								});
 							}
 						});
